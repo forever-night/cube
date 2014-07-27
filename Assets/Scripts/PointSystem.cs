555ade711fe1e781a913389b14c9a50d0a88455d
Anna <anna.upintheair@gmail.com>
2014-07-27 23:44:33 +0300

    changed point & level system

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -4,22 +4,39 @@ using System.Collections;
 public class PointSystem : MonoBehaviour 
 {
 	internal static int points;
+	internal static int level;
 
 
 	void Start () 
 	{
 		points = 0;
+		level = 0;
 	}
 	
 
 	void FixedUpdate () 
 	{
-		 print(points);
+		LevelUp();
+		print("level: " + level + ", points: " + points);
 	}
 
 
-	public static void AddPoints( EnemyMovement enemy )
+	internal static void AddPoints( EnemyMovement enemy )
 	{
 		points += enemy.size;
+	}
+
+
+	internal static void LevelUp()
+	{
+		// DON'T use level++, method is called in FixedUpdate,
+		//		level will be infinitely increasing
+		// DON'T use switch(points),
+		// 		won't give a result if number of points is between two levels
+		level = points == 0 ? 0 : level;
+		level = points >= 2 && points < 6 ? 1 : level;
+		level = points >= 6 && points < 14 ? 2 : level;
+		level = points >= 14 && points < 30 ? 3 : level;
+		level = points >= 30 ? 4 : level;
 	}
 }
