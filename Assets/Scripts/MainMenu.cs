54e4018086e36437d5031c20facdbcfdd6e5ddb6
Anna <anna.upintheair@gmail.com>
2014-08-01 22:22:52 +0300

    [undone] gui

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public GUISkin skin;
+
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+
+		// ugh i'll just leave it like this
+		// nothing is working, anyway
+	}
+}
