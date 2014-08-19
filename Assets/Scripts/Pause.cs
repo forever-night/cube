76d45a8347146294307a4926b6449cba44b2b487
Anna <anna.upintheair@gmail.com>
2014-08-19 13:45:18 +0300

    pause was added

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour 
+{
+    GameObject pause;
+
+
+    void Awake()
+    {
+        pause = GameObject.Find("lblPause");
+    }
+
+	void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+
+                // show pause label
+                Instantiate(pause, new Vector3(0.0F, -1.4F, 0.0F), Quaternion.identity);
+            }
+            else
+            {
+                Time.timeScale = 1;
+
+                // stop showing pause label
+                Destroy(GameObject.Find("lblPause(Clone)"));
+            }
+        }
+    }
+}
